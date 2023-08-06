5a2673fb9c3c0d06be2272af773cf7e03dd34903
Hiren Gohil <gohilashvinbhai324@gmail.com>
2023-08-06 09:57:57 +0530

    Add project files.

---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,14 @@
+﻿namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponse> responses = new();
+        public static IEnumerable<GuestResponse> Responses => responses;
+
+        public static void AddResponse(GuestResponse guestResponse)
+        {
+            Console.WriteLine(responses);
+            responses.Add(guestResponse);
+        }
+    }
+}
